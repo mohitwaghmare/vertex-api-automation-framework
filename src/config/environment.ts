0f645a01783cd50352417b4038641621904a0b6c
mohitwaghmare <208084796+mohitwaghmare@users.noreply.github.com>
2026-08-06 04:40:25 +0530

    Update environment.spec.ts

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,11 +1,11 @@
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const environment = {
-  env: process.env.ENV || 'QA',
-  baseUrl: process.env.BASE_URL || '',
-  apiKey: process.env.API_KEY || '',
-  authToken: process.env.AUTH_TOKEN || '',
-  requestTimeout: Number(process.env.REQUEST_TIMEOUT) || 30000,
+    env: process.env.ENV || 'QA',
+    baseUrl: process.env.BASE_URL || '',
+    apiKey: process.env.API_KEY || '',
+    authToken: process.env.AUTH_TOKEN || '',
+    requestTimeout: Number(process.env.REQUEST_TIMEOUT) || 30000,
 };
